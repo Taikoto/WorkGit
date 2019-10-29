4c8ab096c1821e4b19a71c31e09340a23b2eb9d4
Taikoto <caihuanming@yeah.net>
2019-10-29 23:51:21 +0800

    add slam workspace and some demos

---
+++ slamwork/helloSLAM/helloSLAM.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    cout<<"hello SLAM!\r\n"<<endl;
+    
+    return 0;
+}
---
+++ slamwork/libHelloSLAM/libHelloSLAM.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+//#include "libHelloSLAM.h"
+
+using namespace std;
+
+void printHello()
+{
+    cout<<"Hello SLAM\r\n"<<endl;
+}
